607dd23c899d4054691a36ac15704aa21a076c67
Jason-MacDonald <49774612+Jason-MacDonald@users.noreply.github.com>
2020-02-26 17:13:25 +1300

    Step 6 - Move save and retrieve to clsArtistList

--- Version 1 C/clsArtistList.cs
+++ Version 1 C/clsArtistList.cs
@@ -4,9 +4,11 @@ using System.Windows.Forms;
 
 namespace Version_1_C
 {
-    [Serializable()] 
+    [Serializable()]
     public class clsArtistList : SortedList
     {
+        private const string _FileName = "gallery.xml";
+
         public void EditArtist(string prKey)
         {
             clsArtist lcArtist;
@@ -16,7 +18,7 @@ namespace Version_1_C
             else
                 MessageBox.Show("Sorry no artist by this name");
         }
-       
+
         public void NewArtist()
         {
             clsArtist lcArtist = new clsArtist(this);
@@ -33,7 +35,7 @@ namespace Version_1_C
                 MessageBox.Show("Duplicate Key!");
             }
         }
-        
+
         public decimal GetTotalValue()
         {
             decimal lcTotal = 0;
@@ -42,6 +44,46 @@ namespace Version_1_C
                 lcTotal += lcArtist.GetWorksValue();
             }
             return lcTotal;
+        }
+
+
+        public void Save()
+        {
+            try
+            {
+                System.IO.FileStream lcFileStream = new System.IO.FileStream(_FileName, System.IO.FileMode.Create);
+                System.Runtime.Serialization.Formatters.Soap.SoapFormatter lcFormatter =
+                    new System.Runtime.Serialization.Formatters.Soap.SoapFormatter();
+
+                lcFormatter.Serialize(lcFileStream, this);
+                lcFileStream.Close();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "File Save Error");
+            }
+        }
+
+        public static clsArtistList Retrieve()
+        {
+            clsArtistList lcArtistList = new clsArtistList();
+
+            try
+            {
+                System.IO.FileStream lcFileStream = new System.IO.FileStream(_FileName, System.IO.FileMode.Open);
+                System.Runtime.Serialization.Formatters.Soap.SoapFormatter lcFormatter =
+                    new System.Runtime.Serialization.Formatters.Soap.SoapFormatter();
+
+                lcArtistList = (clsArtistList)lcFormatter.Deserialize(lcFileStream);
+                lcFileStream.Close();
+            }
+
+            catch (Exception e)
+            {
+                lcArtistList = new clsArtistList();
+                MessageBox.Show(e.Message, "File Retrieve Error");
+            }
+            return lcArtistList;
         }
     }
 }
--- Version 1 C/frmMain.cs
+++ Version 1 C/frmMain.cs
@@ -18,8 +18,7 @@ namespace Version_1_C
             InitializeComponent();
         }
 
-        private clsArtistList _ArtistList = new clsArtistList();
-        private const string _FileName = "gallery.xml";
+        clsArtistList _ArtistList;
 
         private void UpdateDisplay()
         {
@@ -51,7 +50,7 @@ namespace Version_1_C
 
         private void btnQuit_Click(object sender, EventArgs e)
         {
-            Save();
+            _ArtistList.Save();
             Close();
         }
 
@@ -68,45 +67,11 @@ namespace Version_1_C
             }
         }
 
-        private void Save()
-        {
-            try
-            {
-                System.IO.FileStream lcFileStream = new System.IO.FileStream(_FileName, System.IO.FileMode.Create);
-                System.Runtime.Serialization.Formatters.Soap.SoapFormatter lcFormatter =
-                    new System.Runtime.Serialization.Formatters.Soap.SoapFormatter();
 
-                lcFormatter.Serialize(lcFileStream, _ArtistList);
-                lcFileStream.Close();
-            }
-            catch (Exception e)
-            {
-                MessageBox.Show(e.Message, "File Save Error");
-            }
-        }
-
-        private void Retrieve()
-        {
-            try
-            {
-                System.IO.FileStream lcFileStream = new System.IO.FileStream(_FileName, System.IO.FileMode.Open);
-                System.Runtime.Serialization.Formatters.Soap.SoapFormatter lcFormatter =
-                    new System.Runtime.Serialization.Formatters.Soap.SoapFormatter();
-
-                _ArtistList = (clsArtistList)lcFormatter.Deserialize(lcFileStream);
-                UpdateDisplay();
-                lcFileStream.Close();
-            }
-
-            catch (Exception e)
-            {
-                MessageBox.Show(e.Message, "File Retrieve Error");
-            }
-        }
 
         private void frmMain_Load(object sender, EventArgs e)
         {
-            Retrieve();
+            _ArtistList = clsArtistList.Retrieve();
             UpdateDisplay();
         }
     }
